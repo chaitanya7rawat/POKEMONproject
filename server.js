1277470e0d1f71bbed86d1e19f119bfbce25b98f
Chaitanya Rawat <charu0519872gmail.com>
2025-04-14 11:14:49 -0700

    done

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = 5000;
 
 // MongoDB connection
 // mongoose.connect("mongodb://localhost:27017/pokemonApp", {
-mongoose.connect("mongodb+srv://chaitanya:CvipKmlZMam0XrUg@cluster0.3nhex46.mongodb.net/pokemonApp?retryWrites=true&w=majority&appName=Cluster0", {
+mongoose.connect("mongodb+srv://chaitanya:ssUyw3YhCOVUsH00cluster0.3nhex46.mongodb.net/pokemonApp?retryWrites=true&w=majority&appName=Cluster0", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
